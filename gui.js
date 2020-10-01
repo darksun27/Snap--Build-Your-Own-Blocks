5721b0fa586afd11f7dd8167f8269a00ac98cec9
Jennifer Tsan <jennifertsan@Jennifers-MacBook-Pro.local>
2020-10-01 14:12:04 -0400

    changed size of speechbubblepanel and agentpanel to give agentpanel more space

--- gui.js
+++ gui.js
@@ -2368,8 +2368,8 @@ IDE_Morph.prototype.fixLayout = function (situation) {
             this.speechBubblePanel.setTop(this.corralBar.bottom());
             this.speechBubblePanel.setLeft(this.stage.left());
             this.speechBubblePanel.setWidth(this.stage.width());
-            if ((this.bottom() - this.corralBar.bottom())*.50 <=360) {
-                this.speechBubblePanel.setHeight((this.bottom() - this.corralBar.bottom())*.50);
+            if ((this.bottom() - this.corralBar.bottom())*.20 <=360) {
+                this.speechBubblePanel.setHeight((this.bottom() - this.corralBar.bottom())*.20);
             } else {
                 this.speechBubblePanel.setHeight(this.bottom() - this.corralBar.bottom() - 360);
             }
@@ -2391,11 +2391,8 @@ IDE_Morph.prototype.fixLayout = function (situation) {
             this.agentPanel.setPosition(this.speechBubblePanel.bottomLeft());
             this.agentPanel.setLeft(this.stage.left());
             this.agentPanel.setWidth(this.stage.width());
-            if ((this.bottom() - this.corralBar.bottom())*.50 <=360) {
-                this.agentPanel.setHeight((this.bottom() - this.corralBar.bottom())*.50);
-            } else {
-                this.agentPanel.setHeight(360);
-            }
+            this.agentPanel.setHeight(360);
+
         }
 
         // corralBar
